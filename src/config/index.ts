08c77861ea58215128c7bf95885f3d426c295ba8
habibimamrifat <habibrifatx21@gmail.com>
2025-01-15 14:42:40 +0600

    password hashed

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,7 @@ dotenv.config({path:path.join(process.cwd(),".env")})
 
 const config ={
 port:process.env.port,
-Mongoose_uri:process.env.DBUri
+Mongoose_uri:process.env.DBUri,
+BcryptSalt:process.env.BcryptSalt
 }
 export default config
--- src/modules/users/user.model.ts
+++ src/modules/users/user.model.ts
@@ -1,26 +1,46 @@
 import { model, Schema } from "mongoose";
 import { Tuser } from "./user.interface";
+import bcrypt from "bcrypt";
+import config from "../../config";
 
-const userSchema=new Schema<Tuser>({
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    role:{
-        type:String,
-        enum:["admin","user"],
-        default:"user"
-    }
-})
+const userSchema = new Schema<Tuser>({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+});
 
-const userModel=model("User",userSchema);
+userSchema.pre("save", async function (next) {
+  const user = this;
+  const existingUser = await userModel.findOne({ email: user.email });
+  if (existingUser) {
+    const error = new Error("Email already exists");
+    return next(error);
+  }
+  next();
+});
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  user.password = await bcrypt.hash(user.password, Number(config.BcryptSalt));
+
+  next();
+});
+
+const userModel = model("User", userSchema);
 export default userModel;
